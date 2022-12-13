697d84c8518f35be9652d8dc775063b5038fcdee
Austin Holmes <austin.holmes4@gmail.com>
2022-12-12 21:14:13 -0700

    app done, need to debug owner name not showing on list

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from forms import AddForm, DelForm
-from flask import Flask, render_template, url_for, redirect, request
+from forms import AddForm, DelForm, OwnerForm
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -27,15 +27,35 @@ Migrate(app, db)
 class Puppy(db.Model):
 
     __tablename__ = 'puppies'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name is {self.name}"
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is {self.owner.name}"
+        else:
+            return f"Puppy name is {self.name} and has no owner"
 
+
+class Owner(db.Model):
+    
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"Owner name is {self.name} "
 ##########################
 ### View Functions #######
 ##########################
@@ -55,7 +75,7 @@ def add_pup():
             new_pup = Puppy(name)
             db.session.add(new_pup)
             db.session.commit()
-            print('test')
+
             return redirect(url_for('list_pup'))
 
 
@@ -81,6 +101,26 @@ def del_pup():
 
         return redirect(url_for('list_pup'))
     return render_template('delete.html', form=form)
+
+@app.route('/owner', methods=["GET", "POST"])
+def add_owner():
+
+    form = OwnerForm()
+
+    if form.validate_on_submit():
+
+        name = form.name.data
+        pup_id = form.pup_id.data
+
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+        flash(f"{form.name.data} has been added")
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('owner.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -4,10 +4,16 @@ from wtforms.validators import DataRequired
 
 class AddForm(FlaskForm):
 
-    name = StringField('Name of Puppy: ')
+    name = StringField('Name of Puppy:')
     submit = SubmitField('Add a Puppy')
 
 class DelForm(FlaskForm):
 
-    id = IntegerField("Id Number of Puppy to Remove: ")
+    id = IntegerField("Id Number of Puppy to Remove:")
     submit = SubmitField("Remove puppy")
+
+class OwnerForm(FlaskForm):
+
+    name = StringField('Name of Owner:')
+    pup_id = IntegerField('ID of Puppy:')
+    submit = SubmitField('Add Owner')
